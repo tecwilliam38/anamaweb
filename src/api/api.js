a5a3097c5a18a3024e273adadf6bd60186d94239
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-19 07:52:43 -0300

    vercel

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 
-const VERCELAPI = "https://ans-apitech.vercel.app";
+const VERCELAPI = "https://apianama.vercel.app";
 
 const LOCALHOST = "http://192.168.1.65:3001";
 const TESTEROUTE = "http://localhost:3001";
 
 const api = axios.create({
-    baseURL: TESTEROUTE,
+    baseURL: VERCELAPI,
 });
 
 export default api;
